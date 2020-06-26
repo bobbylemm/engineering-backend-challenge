2e4ebda899e91bca46d0dbecd1b6ed594c329751
bobbylemm <chidozienwoga@gmail.com>
2020-06-26 23:27:46 +0100

    fix env issue

--- config/config.js
+++ config/config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   "production": {
     "url": process.env.DATABASE_URL,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "use_env_variable": "production"
   }
 }
